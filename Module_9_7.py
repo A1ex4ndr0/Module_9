2be6c0eafb5bcaabb193c86d7be5a7c8ac80b947
A1ex4ndr0 <a1ex.n.brb@gmail.com>
2025-01-08 20:45:27 +0300

    Декораторы

---
+++ Module_9_7.py
@@ -0,0 +1,25 @@
+from sys import flags
+
+
+def is_prime(func):
+    def wrapper(*args):
+        res = func(*args)
+        flag = True
+        for i in range(2, int(res**0.5+1)):
+            if res % i == 0:
+                flag = False
+        if flag:
+            print('Число простое')
+        else:
+            print('Число составное')
+        return res
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 7)
+print(result)
